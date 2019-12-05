bb750aea72adb2169dd3fc075df88fb495ee2bee
charantej224 <charantej.career@gmail.com>
2019-12-05 10:51:01 -0600

    Assignment 2

---
+++ src/main/java/com/algorithms/BucketSort.java
@@ -0,0 +1,53 @@
+package com.algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BucketSort {
+
+    public static void main(String[] args) {
+
+        int[] input = {1, 4, 1, 2, 7, 5, 2};
+        System.out.println(Arrays.toString(input));
+        bucketSort(input);
+        System.out.println(Arrays.toString(input));
+
+    }
+
+    public static void bucketSort(int[] input) {
+        final int[] code = hash(input);
+        List[] buckets = new List[code[1]];
+        for (int i = 0; i < code[1]; i++) {
+            buckets[i] = new ArrayList();
+        }
+        for (int i : input) {
+            buckets[hash(i, code)].add(i);
+        }
+        for (List bucket : buckets) {
+            Collections.sort(bucket);
+        }
+        int ndx = 0;
+        for (int b = 0; b < buckets.length; b++) {
+            for (Object v : buckets[b]) {
+                input[ndx++] = (int) v;
+            }
+        }
+    }
+
+    private static int[] hash(int[] input) {
+        int m = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (m < input[i]) {
+                m = input[i];
+            }
+        }
+        return new int[]{m, (int) Math.sqrt(input.length)};
+    }
+
+    private static int hash(int i, int[] code) {
+        return (int) ((double) i / code[0] * (code[1] - 1));
+    }
+
+}
---
+++ src/main/java/com/algorithms/CountingSort.java
@@ -0,0 +1,34 @@
+package com.algorithms;
+import java.util.Arrays;
+
+public class CountingSort {
+
+    public static void main(String[] args){
+        int[] inputArray = {1, 4, 1, 2, 7, 5, 2};
+        System.out.println(Arrays.toString(sort(inputArray, 7)));
+    }
+
+    private static int[] countElements(int[] input, int k) {
+        int[] c = new int[k + 1];
+        Arrays.fill(c, 0);
+        for (int i : input) {
+            c[i] += 1;
+        }
+        for (int i = 1; i < c.length; i++) {
+            c[i] += c[i - 1];
+        }
+        return c;
+    }
+
+    private static int[] sort(int[] input, int k) {
+        int[] c = countElements(input, k);
+        int[] sorted = new int[input.length];
+        for (int i = input.length - 1; i >= 0; i--) {
+            int current = input[i];
+            sorted[c[current] - 1] = current;
+            c[current] -= 1;
+        }
+        return sorted;
+    }
+
+}
